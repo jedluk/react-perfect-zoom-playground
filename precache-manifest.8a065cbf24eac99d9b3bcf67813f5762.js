e17ee2da2d319590920629b855e16de9c40b97a4
Jedrzej Lukasiuk <jedrzej.lukasiuk@gmail.com>
2019-12-28 00:13:14 +0100

    Updates

---
+++ precache-manifest.8a065cbf24eac99d9b3bcf67813f5762.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c52d5e96579a4af3924e8b208f7311d5",
+    "url": "/react-perfect-zoom-playground/index.html"
+  },
+  {
+    "revision": "9049e79e0c0cd7ff87fd",
+    "url": "/react-perfect-zoom-playground/static/css/2.8da71afa.chunk.css"
+  },
+  {
+    "revision": "a2d766272171b9768ce7",
+    "url": "/react-perfect-zoom-playground/static/css/main.1675394c.chunk.css"
+  },
+  {
+    "revision": "9049e79e0c0cd7ff87fd",
+    "url": "/react-perfect-zoom-playground/static/js/2.031c2ea6.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/react-perfect-zoom-playground/static/js/2.031c2ea6.chunk.js.LICENSE"
+  },
+  {
+    "revision": "a2d766272171b9768ce7",
+    "url": "/react-perfect-zoom-playground/static/js/main.11e530f0.chunk.js"
+  },
+  {
+    "revision": "71524eb36824471244af",
+    "url": "/react-perfect-zoom-playground/static/js/runtime-main.aad62880.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-perfect-zoom-playground/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "05cb93df36d2ca216599bd235630e888",
+    "url": "/react-perfect-zoom-playground/static/media/sample2.05cb93df.jpg"
+  }
+]);
