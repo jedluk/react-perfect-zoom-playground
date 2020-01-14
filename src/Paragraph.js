95b9b2b97194ba132c811d7fe241127fc3294295
Jedrzej Lukasiuk <jedrzej.lukasiuk@gmail.com>
2020-01-14 22:40:11 +0100

    tweaked Paragraph component

--- src/Paragraph.js
+++ src/Paragraph.js
@@ -1,14 +1,13 @@
 import React from 'react';
 
-const Paragraph = props => (
-  <p>
-    Sed a rhoncus elit. Pellentesque ultrices vestibulum velit, quis pellentesque leo sagittis non.
+const Paragraph = ({ length = 1 }) => {
+  const text = `Sed a rhoncus elit. Pellentesque ultrices vestibulum velit, quis pellentesque leo sagittis non.
     Integer vel dictum orci. Praesent ac interdum massa. Aenean felis dui, aliquam nec consectetur
     non, rutrum eget diam. Proin id lorem eu est commodo condimentum et ac arcu. Integer ut libero
     id nibh aliquet gravida in sit amet neque. Donec nec justo ligula. Vestibulum eget tortor
     tempor, suscipit lorem a, luctus neque. Nam venenatis tortor nisl, sit amet fermentum nibh
-    imperdiet a.
-  </p>
-);
+    imperdiet a.`;
+  return <p style={{ paddingLeft: 20, paddingRight: 20 }}>{text.repeat(length)}</p>;
+};
 
 export default Paragraph;
