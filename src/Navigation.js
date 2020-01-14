5da075f61e913b7cfb19e538164418be1163ec15
Jedrzej Lukasiuk <jedrzej.lukasiuk@gmail.com>
2020-01-14 23:54:42 +0100

    fixed bad links in navigation

--- src/Navigation.js
+++ src/Navigation.js
@@ -1,16 +1,18 @@
 import React from 'react';
 import { Link, withRouter } from 'react-router-dom';
 
+const { PUBLIC_URL = '' } = process.env;
 const activeClass = (path, activePath) => (activePath === path ? 'nav-active' : undefined);
+
 const Navigation = props => {
   const { pathname: activePath } = props.location;
   return (
     <ul className="navigation">
-      <li className={activeClass('/', activePath)}>
-        <Link to="/">Controller</Link>
+      <li className={activeClass(`${PUBLIC_URL}/`, activePath)}>
+        <Link to={`${PUBLIC_URL}/`}>Controller</Link>
       </li>
-      <li className={activeClass('/demo', activePath)}>
-        <Link to="/demo">Two sources</Link>
+      <li className={activeClass(`${PUBLIC_URL}/demo`, activePath)}>
+        <Link to={`${PUBLIC_URL}/demo`}>Two sources</Link>
       </li>
     </ul>
   );
